cbdac30968a0b017a6fca67e2e36b2f97fd15dcd
Anastasia933 <anastasia.warken@stud.uni-heidelberg.de>
2023-04-28 14:05:16 +0200

    Import und reshaping der CSV
    Import der Zalando MNIST CSV Datei, damit das bei allen funktioniert die Kommentare lesen und notwendige Pakete herunterladen + CSV Datei am richtigen Ort speichern!

--- machine_leaning.py
+++ machine_leaning.py
@@ -1,3 +1,46 @@
-# This is where the code belongs
-pip install numpy
-import numpy
+# Pakete etc. 
+# Pandas, Numpy und Matplotlib davor über Miniforge Prompt herunterladen sonst geht hier gar nix
+
+import numpy as np
+import pandas as pnd
+import matplotlib.pyplot as plt
+
+
+# Import der Dateien 
+# ANMERKUNG: Funktioniert auch nur, wenn die Dateien in der SELBEN Directory wie das Skript sind -> Im Git-Ordner
+# Problem mit Git da CSV-Dateien zu groß sind um sie zu committen, sonst kann man gar nix mehr pushen 
+# Dateien in GitHub Desktop ab in .gitignore, dann verpisst sich das Problem
+
+testdata = pnd.read_csv('fashion-mnist_test.csv')
+traindata = pnd.read_csv('fashion-mnist_train.csv')
+
+# Extraktion der Pixel und Label aus der CSV Datei, 'reshaping' der CSV Datei als 28 x 28 Bild (img)
+
+pixel = traindata.drop('label', axis=1).to_numpy()
+label = traindata['label'].to_numpy()
+img = pixel.reshape((-1, 28, 28))
+
+# Wer sich die Bilder mal anschauen will: 
+
+#anz_img = len(img)
+#rows = int(np.sqrt(anz_img))
+#cols = int(np.ceil(anz_img / rows))
+#rows = np.minimum(rows, 20)
+#cols = np.minimum(cols, 20)
+#fig, axes = plt.subplots(rows, cols, figsize=(10, 10))
+#for i, ax in enumerate(axes.flat):
+#    if i < anz_img:
+#        ax.imshow(img[i], cmap='gray')
+#        ax.set_axis_off()
+#    else:
+#        ax.set_axis_off()
+
+#plt.show()
+
+
+
+
+
+
+
+
