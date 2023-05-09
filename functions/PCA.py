fcff279d3e449ba9efdefd594a40cc336c68f7aa
Anastasia933 <anastasia.warken@stud.uni-heidelberg.de>
2023-05-09 15:01:29 +0200

    Transformed

--- functions/PCA.py
+++ functions/PCA.py
@@ -7,7 +7,7 @@ def centererd(): # 'centering ist ein python eigenname, das Python Modul wird hi
     centered_img = img - img.mean(axis=(1,2), keepdims=True) #mean is calculated along 2nd and 3rd axis, meaning the heigth and the width of an image (remember: the first dimension is which picture we are looking at)
     return centered_img
 
-def pca(centered_img, prop_var): #added input variable centered_img
+def pca(centered_img, prop_variance): #added input variable centered_img
     covariance_matrix = np.cov(centered_img.reshape(num_img, -1), rowvar=False) #defining variable and creating covariance matrix while reshaping/flattening the images to obtain a 2D array, rowvar = False to compute cov matrix over rows, -1 means that the function calculates the required dimensions, so if the number of pictures is given it calculates the size 28x28 for each picture
     eigen_val , eigen_vec = np.linalg.eigh(covariance_matrix) #eigh function has two outputs, so two values have to be defined
     sorted_index = np.argsort(eigen_val)[::-1] #gives indexes to sort array from lowes to highest and inverts this vector
