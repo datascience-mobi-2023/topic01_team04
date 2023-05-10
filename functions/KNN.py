1b892248d36da7ce377ae1764c7d6fed721c9b61
DerSchredder42 <66201200+DerSchredder42@users.noreply.github.com>
2023-05-10 20:02:53 +0200

    KNN almost done

--- functions/KNN.py
+++ functions/KNN.py
@@ -6,11 +6,15 @@ import matplotlib.pyplot as plt
 
 def dist(PCs_test,PCs_train,k):
     for i in range(0,len(PCs_test)):
+        
         result = []
         for y in range(0,len(PCs_train)):
-            result = numpy.append (result, [np.linalg.norm(PCs_test[i]-PCs_train[y])]) 
-            print(result)
-            classes = np.argsort(result)
-            print(classes)
-            return classes
+            result = np.append(result, [np.linalg.norm(PCs_test[i]-PCs_train[y])])
+
+        #print(result)
+        classes = np.argsort(result)
+        class_k = classes[:int(k)]
+        print(class_k)
+        
+    return class_k
 
