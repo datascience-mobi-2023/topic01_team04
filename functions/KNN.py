94c94cacd2e30ce0d4e44f511d8e51bee2235db6
DerSchredder42 <66201200+DerSchredder42@users.noreply.github.com>
2023-05-10 14:43:20 +0200

    KNN

--- functions/KNN.py
+++ functions/KNN.py
@@ -1,2 +1,14 @@
-def dist(img1,img2):
-    
+import numpy as np
+import pandas as pnd
+import matplotlib.pyplot as plt
+
+
+
+def dist(PCs_test,PCs_train):
+    for i in range(0,len(PCs_test)):
+        result = []
+        for y in range(0,len(PCs_train)):
+            result += [np.linalg.norm(PCs_test[i]-PCs_train[y])]
+            classes = np.argsort(result)
+            return classes
+
