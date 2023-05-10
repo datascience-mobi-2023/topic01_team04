cbcc5ce19f9ce1fa2775204f74446abc42b358df
Bastian Mucha <bastian.mucha@gmail.com>
2023-05-10 14:04:53 +0200

    cleanup of PCA.py

--- functions/PCA.py
+++ functions/PCA.py
@@ -1,23 +1,19 @@
 import numpy as np
 import pandas as pnd
 import matplotlib.pyplot as plt
-#import main #No module named 'main'
 
-def centered(img): # 'centering ist ein python eigenname, das Python Modul wird hiermit überschrieben. Das kann zu Problemen führen.'
-    centered_img = img - img.mean(axis=(1), keepdims=True) #mean is calculated along 2nd and 3rd axis, meaning the heigth and the width of an image (remember: the first dimension is which picture we are looking at)
+def centered(img): #centering ist ein python eigenname, das Python Modul wird hiermit überschrieben. Das kann zu Problemen führen.
+    centered_img = img - img.mean(axis=(1), keepdims=True) #mean is calculated along the horizontal axis
     return centered_img
 
-def pca(centered_img, prop_variance): #added input variable centered_img
-    #centered_img = np.reshape(centered_img,(-1,1)) #gives array of shape (784,1) #needs to me modifyed
-    #print(centered_img.shape)
-    covariance_matrix = np.cov(centered_img, rowvar=False) #defining variable and creating covariance matrix while reshaping/flattening the images to obtain a 2D array, rowvar = False to compute cov matrix over rows, -1 means that the function calculates the required dimensions, so if the number of pictures is given it calculates the size 28x28 for each picture
-    print(covariance_matrix)
+def pca(centered_img, prop_variance):
+    covariance_matrix = np.cov(centered_img, rowvar=False) #covariance matrix, as each column is a variable we need rowvar=False
     eigen_val , eigen_vec = np.linalg.eigh(covariance_matrix) #eigh function has two outputs, so two values have to be defined
     sorted_index = np.argsort(eigen_val)[::-1] #gives indexes to sort array from lowes to highest and inverts this vector
     sorted_eigenvalue = eigen_val[sorted_index] #apply sorting to eigenvalues
     print(len(sorted_eigenvalue)) #works until here
     sorted_eigenvectors = eigen_vec[:,sorted_index] #apply sorting to eigenvectors, first coordinate (vertical axis) has to be : to select all rows
-    def propvar(prop_var):
+    def propvar(prop_var): #adds eigenvalues until desired proportion of variance is reached
         sum = 0
         sum_eigenvalues = np.sum(eigen_val)
         principal_component_number = 0
