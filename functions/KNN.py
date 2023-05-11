f4a4bb172ab66e830bce72c279cb1a17570ab3dd
DerSchredder42 <66201200+DerSchredder42@users.noreply.github.com>
2023-05-11 14:00:24 +0200

    stuff

--- functions/KNN.py
+++ functions/KNN.py
@@ -14,9 +14,11 @@ def dist(PCs_test,PCs_train,k):
         result = np.array([],dtype=np.int8)
         for y in range(0,len(PCs_train)):
             result = np.append(result, [np.linalg.norm(PCs_test[i]-PCs_train[y])])
-
-        #print(result)
+        
         class_k = np.argsort(result)[:k]
+        print(result[class_k])
+        print(result.shape)
+        
         
         
         final_result[i] = class_k
