1b60e155ea59aaa22f910d5f8766190a57e1d4f6
Bastian Mucha <bastian.mucha@gmail.com>
2023-05-15 16:26:57 +0200

    cleanup

--- functions/PCA.py
+++ functions/PCA.py
@@ -55,5 +55,29 @@ def custum_imshow(img):
         else:
             ax.set_axis_off()
     plt.show()
+    
+def testfunction():
+    #gaining input for testing all the funktions:
+    i = False
+    choice = int(input('1. test PCA\n2. test KNN: '))
+    #match inbud (was passiert hier? syntax hat nicht funktioniert)
+    if  choice == 1: #testing out to PCA
+        print('shape of the training Data: ' + str(traindata_centered.shape))
+        while i == False:
+            print('\n')
+            eiovar = input('type either eigenvector number or explained proportion of variance: ')
+            print('shape of the training PCA: ' + str(pca(traindata_centered, float(eiovar))[0].shape))
+            if input('again? y/n: ') == 'n':
+                i = True
+    elif choice == 2: #Testing out the KNN-Method
+        while i == False:
+            print('\n')
+            k = input('whats k?: ')
+            item_numbers_of_most_similar_pics = dist(pca(centered(testdata_pixel), 10)[0], pca(test, 10)[0], k)
+            print(str(most_common_items(labl(item_numbers_of_most_similar_pics,label))))
+            print(str(label_test[:10]))
+            print(str(quality(label_test[:10], most_common_items(labl(item_numbers_of_most_similar_pics,label)))))
+            if input('again? y/n: ') == 'n':
+                i = True
 
     
--- main.py
+++ main.py
@@ -8,12 +8,8 @@ import pandas as pnd
 import matplotlib.pyplot as plt
 import sklearn as skl
 
-from functions.PCA import pca
-from functions.PCA import centered
-from functions.KNN import dist
-from functions.KNN import labl
-from functions.KNN import most_common_items
-from functions.KNN import quality
+from functions.PCA import *
+from functions.KNN import *
 
 from sklearn.decomposition import PCA
 print('\n')
@@ -35,39 +31,6 @@ num_img = traindata_pixel.shape[0]
 
 # subtracting mean of each pixel while keeping the dimensions of the images to center the images in preparation for PCA
 traindata_centered = centered(traindata_pixel)
+testfunction()
 
-#gaining input for testing all the funktions:
-i = False
-inbud = int(input('1. test PCA\n2. test KNN: '))
-#match inbud (was passiert hier? syntax hat nicht funktioniert)
-if  inbud == 1: #testing out to PCA
-    print('shape of the training Data: ' + str(test.shape))
-    i = False
-    while i == False:
-        print('\n')
-        
-        eiovar = input('type either eigenvector number or explained proportion of variance: ')
-        print('shape of the training PCA: ' + str(pca(test, float(eiovar))[0].shape))
-        
-        if input('again?: ') == 'no':
-            i = True
-elif inbud == 2: #Testing out the KNN-Method
-    while i == False:
-        print('\n')
 
-        k = input('whats k?: ')
-        item_numbers_of_most_similar_pics = dist(pca(centered(testdata_pixel), 10)[0], pca(test, 10)[0], k)
-        
-        print(str(most_common_items(labl(item_numbers_of_most_similar_pics,label))))
-        print(str(label_test[:10]))
-        print(str(quality(label_test[:10], most_common_items(labl(item_numbers_of_most_similar_pics,label)))))
-
-        if input('again?: ') == 'no':
-            i = True
-
-def parameter_input():
-    choice = int(input('1. test PCA\n2. test KNN: '))
-    match choice:
-        case 1:
-            print('shape of the training Data: ' + str(traindata_centered.shape))
-        case 2:
